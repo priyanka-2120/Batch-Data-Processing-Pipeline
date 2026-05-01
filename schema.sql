9c6b8268b3522ca22d94350b8a52974a4305175d
V PRIYANKA <charypriyanka21@gmail.com>
2026-05-01 12:30:54 +0530

    Create database and youtube_data table

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE data_engineering;
+
+USE data_engineering;
+
+CREATE TABLE youtube_data (
+    video_id VARCHAR(50),
+    views INT,
+    likes INT,
+    comments INT,
+    total_engagement INT
+);
